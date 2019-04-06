d04a96561cec047dbfbd54e9a2b0fd143986146e
Leonardo F <leonardof@tutanota.com>
2019-04-06 00:01:29 -0300

    a

---
+++ URI/1_Iniciante/1004.py
@@ -0,0 +1,6 @@
+var1 = int(input())
+var2 = int(input())
+
+PROD = var1 * var2
+
+print("PROD = {}" .format(PROD))
---
+++ URI/1_Iniciante/1005.py
@@ -0,0 +1,6 @@
+nota_a = float(input())
+nota_b = float(input())
+
+MEDIA = ((nota_a*3.5) + (nota_b*7.5) / 11)
+
+print("PROD = {0:.2f}" .format(MEDIA))
